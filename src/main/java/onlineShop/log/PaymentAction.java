d63187ea195ba43b9ba3350f228215d682dbd45e
Zifan Zhang <zifanzhang@Somnus.fios-router.home>
2020-03-31 00:02:53 -0400

    2020.03.30

--- src/main/java/onlineShop/log/PaymentAction.java
+++ src/main/java/onlineShop/log/PaymentAction.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+//declare a bean
 public class PaymentAction {
 	@Autowired
+	//use the bean
 	private Logger logger;
 
 	public void pay(BigDecimal payValue) {
